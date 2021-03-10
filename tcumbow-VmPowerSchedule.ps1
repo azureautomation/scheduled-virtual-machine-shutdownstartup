16590e2ee4ad9139285544fa46162c29513af85c
Tom Cumbow <tcumbow@icloud.com>
2021-03-10 13:56:07 -0500

    replaced authentication routine with modern stuff

--- tcumbow-VmPowerSchedule.ps1
+++ tcumbow-VmPowerSchedule.ps1
@@ -293,44 +293,16 @@ try
         }
     }
 
-    # Retrieve credential
-    write-output "Specified credential asset name: [$AzureCredentialName]"
-    if($AzureCredentialName -eq "Use *Default Automation Credential* asset")
-    {
-        # By default, look for "Default Automation Credential" asset
-        $azureCredential = Get-AutomationPSCredential -Name "Default Automation Credential"
-        if($azureCredential -ne $null)
-        {
-		    Write-Output "Attempting to authenticate as: [$($azureCredential.UserName)]"
-        }
-        else
-        {
-            throw "No automation credential name was specified, and no credential asset with name 'Default Automation Credential' was found. Either specify a stored credential name or define the default using a credential asset"
-        }
-    }
-    else
-    {
-        # A different credential name was specified, attempt to load it
-        $azureCredential = Get-AutomationPSCredential -Name $AzureCredentialName
-        if($azureCredential -eq $null)
-        {
-            throw "Failed to get credential with name [$AzureCredentialName]"
-        }
-    }
-
-    # Connect to Azure using credential asset (classic API)
-    $account = Add-AzureAccount -Credential $azureCredential
-	
-    # Check for returned userID, indicating successful authentication
-    if(Get-AzureAccount -Name $azureCredential.UserName)
-    {
-        Write-Output "Successfully authenticated as user: [$($azureCredential.UserName)]"
-    }
-    else
-    {
-        throw "Authentication failed for credential [$($azureCredential.UserName)]. Ensure a valid Azure Active Directory user account is specified which is configured as a co-administrator (using classic portal) and subscription owner (modern portal) on the target subscription. Verify you can log into the Azure portal using these credentials."
-    }
-
+    # Authentication and connection
+    $connectionName = "AzureRunAsConnection"
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+    Add-AzAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+    Write-Output "Successfully logged into Azure subscription using Az cmdlets..."
+    
     # Validate subscription
     $subscriptions = @(Get-AzureSubscription | where {$_.SubscriptionName -eq $AzureSubscriptionName -or $_.SubscriptionId -eq $AzureSubscriptionName})
     if($subscriptions.Count -eq 1)
